1341adbc45325372d3f372e2985a27ffa0418b8b
Alexandre <vensalex1991@gmail.com>
2023-11-24 15:35:01 +0100

    finish project todo?

--- src/components/gettingStarted.jsx
+++ src/components/gettingStarted.jsx
@@ -1,28 +1,37 @@
 import DisplayList from "./listTodo.jsx";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
+
 
 const GettingStarted = () => {
-    const [todo, setTodo] = useState([])
+    const [todo, setTodo] = useState(() => {
+        const storedTodos = JSON.parse(window.localStorage.getItem("todos"));
+        return storedTodos || [];
+    });
+    const inputRef = useRef();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const addTodo = document.getElementById("inputTodo");
-        setTodo([...todo, addTodo.value]);
+        const addTodo = inputRef.current;
+        setTodo([...todo, {text:addTodo.value, completed:false}]);
         addTodo.value = "";
     }
+
+    useEffect(() => {
+        window.localStorage.setItem("todos", JSON.stringify(todo))
+    }, [todo])
 
 
     return(
       <>
           <section className={"mx-auto w-3/4 flex justify-center border-b-2 pb-4 md:justify-start"}>
               <form className={"flex flex-col w-52 justify-center"} onSubmit={handleSubmit}>
-                  <input className={"rounded px-2 mb-5 shadow"} type={"text"} placeholder={"Type a new todo"}
+                  <input ref={inputRef} className={"rounded px-2 mb-5 shadow"} type={"text"} placeholder={"Type a new todo"}
                          id={"inputTodo"}/>
                   <input className={"rounded bg-blue-500 font-bold text-amber-50"} type={"submit"} id={"addTodo"}/>
               </form>
           </section>
           <section className={"flex flex-col mx-auto w-3/4"}>
-              <DisplayList todo={todo}/>
+              <DisplayList todos={todo} setTodos={setTodo}/>
           </section>
       </>
   )
--- src/components/listTodo.jsx
+++ src/components/listTodo.jsx
@@ -1,25 +1,52 @@
 import PropTypes from "prop-types";
+import {useEffect, useRef, useState} from "react";
 
-const DisplayList = (props) => {
+const DisplayList = ({todos, setTodos}) => {
+
+    const inputCheckRef = useRef();
+    const inputDelRef = useRef();
+
+    const handleChecked = (id) =>{
+        const newTodos = [...todos]; // Create a shallow copy of todos
+        newTodos[id].completed = !newTodos[id].completed;
+        setTodos(newTodos);
+    }
+
+    const handleDel = (event) =>{
+        event.preventDefault();
+        const newTodos = todos.filter((value) => !value.completed);
+        setTodos(newTodos);
+
+    }
 
     return (
         <>
-            <h2>Todos</h2>
-            {props.todo.map((value, index) => (
-            <div className={"mx-auto flex flex-col justify-center md:mx-0"} key={index}>
-                <div  className={"flex mt-4 md:mx-0 "}>
-                    <input type={"checkbox"} className={"w-5 mr-4"}/>
-                    <p>{value}</p>
-                </div>
-            </div>
-            ))}
+            <form>
+                <h2 className={"font-bold text-3xl"}>Todos</h2>
+                {todos.map((value, index) => (
+                    <div className={"mx-auto flex flex-col justify-center md:mx-0"} key={index}>
+                        <div className={"flex mt-4 md:mx-0 "}>
+                            <input
+                                ref={inputCheckRef}
+                                onClick={() =>handleChecked(index)}
+                                checked={!!value.completed}
+                                type={"checkbox"}
+                                className={"w-5 mr-4"}/>
+                            <p>{value.text}</p>
+                        </div>
+                    </div>
+                ))}
+                <input className={"px-4 mt-4 font-bold rounded bg-blue-500 text-amber-50"}
+                       type={"submit"}
+                       onClick={handleDel} value={"Delete"}/>
+            </form>
         </>
 
     )
 
-
 }
 DisplayList.propTypes = {
-    todo: PropTypes.array.isRequired
+    todos: PropTypes.array.isRequired,
 };
+
 export default DisplayList;
